beb71b09870aded9295c867f71d77d811c4fd501
ggoswami777 <gaurav.goswami1304@gmail.com>
2025-10-20 17:58:39 +0530

    did mini assignments

---
+++ week3/jwt/jwt.js
@@ -0,0 +1,43 @@
+const jwt=require('jsonwebtoken');
+const jwtPassword="secret";
+const zod=require("zod");
+
+const emailSchema=zod.string().email();
+const passwordSchema=zod.string().min(6);
+
+function signJwt(username,password){
+    const usernameResponse=emailSchema.safeParse(username);
+    const passwordSchema=passwordSchema.safeParse(password);
+    if (!usernameResponse.success || !passwordResponse.success) {
+        return null;
+    }
+    const signature=jwt.sign({
+        username
+    },jwtPassword);
+    return signature;
+}
+
+function verifyJwt(token){
+    let ans=true;
+    try{
+        jwt.verify(token,jwtPassword);
+    }catch(e){
+        ans=false;
+    }
+    return ans;
+}
+
+function decodeJwt(token){
+    const decoded=jwt.decode(token);
+    if(decoded){
+        return true;
+    }else{
+        return false;
+    }
+}
+module.exports = {
+    signJwt,
+    verifyJwt,
+    decodeJwt,
+    jwtPassword
+}
